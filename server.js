cce91af2b1c9c2376330e1fe0ca5340e58b44321
sntpwbl <joaop401@outlook.com>
2024-05-09 11:23:33 -0300

    template inicial

---
+++ server.js
@@ -0,0 +1,22 @@
+//usar npm start para rodar c/ nodemon
+
+//configuração do ambiente, importando o arquivo .env, módulos express, mongoose e cors
+require('dotenv').config({path: './config/.env'})
+
+const express = require('express')
+const app = express()
+
+const mongoose = require('mongoose')
+
+const cors = require('cors')
+app.use(cors())
+
+//configuração dos middlewares
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+//teste home
+app.get('/', (_, res) => res.send('teste'))
+
+
+app.listen(3000, () => console.log('App rodando na porta 3000'))
