dbdd2087a3f45c51e1b8aa91a0b9e8711dcb0a88
Erica Lelis <116844017+zarquells@users.noreply.github.com>
2024-05-20 11:48:35 -0300

    Update pessoaRotas.js

--- routes/pessoaRotas.js
+++ routes/pessoaRotas.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const pessoaController = require('../controller/pessoaControler')
 
+router.get('/',    (req, res) => pessoaController.lerTodasPessoas  (req, res))
 
+router.get('/:id', (req, res) => pessoaController.lerPessoaId      (req, res))
+
+router.post('/',   (req, res) => pessoaController.inserirNovaPessoa(req, res))
+
+router.put('/',   (req, res)  => pessoaController.atualizarPessoa  (req, res))
+
+router.delete('/', (req, res) => pessoaController.deletarPessoa    (req, res))
 
 module.exports = router
